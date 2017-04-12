c459ce4a41f670b93c8ef30dc82be9ea1e7f7f47
David Lorente <david.l.castilla@gmail.com>
2017-04-11 22:12:14 -0400

    create init function

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -5,9 +5,35 @@ chrome.runtime.onMessage.addListener(
     switch(request.action) {
     case "init":
         console.log("init")
+        init();
         break;
     default:
         console.log("defaut")
 		}
   }
 );
+
+const init = () => {
+	console.log($("*"))
+	$("*").mouseenter(function(evt){   
+		evt.stopPropagation();
+		$(this).addClass( "ez-reader-focused" );
+    $(this).css({"border-color": "#C1E0FF", 
+             "border-width":"5px", 
+             "border-style":"solid"});
+	});
+
+	$("*").mouseout(function () {
+		console.log("mouseout unbind")
+		// $(this).removeAttr('style');
+    $(this).css({ "border-color": "inherit",
+            "border-width": "inherit",
+            "border-style": "inherit" });
+    });
+		// $('.ez-reader-focused').off('click');
+
+	// $('.ez-reader-focused').on('click', function(evt){
+ //    	evt.stopPropagation();
+ //    	console.log("CLICK STOP")
+ //    })
+}
