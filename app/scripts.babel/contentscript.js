69209bd0c96f0ea353945cccdb1e7c13fbe20abf
david <david.l.castilla@gmail.com>
2017-04-19 16:11:55 -0400

    move styles to stylesheet

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,49 +3,34 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     switch(request.action) {
-    case "init":
-        console.log("init")
+    case 'init':
+        console.log('init')
         init();
         break;
     default:
-        console.log("defaut")
+        console.log('defaut')
 		}
   }
 );
 
 const init = () => {
-	console.log($("*"))
-  $("*").mouseenter(function (evt) {
+	console.log($('*'))
+  $('*').mouseenter(function (evt) {
     evt.stopPropagation();
-    $(this).css({ "border-color": "#C1E0FF",
-      "border-width": "5px",
-      "border-style": "solid" });
+    $(this).addClass('ezrdr-focused');
     $(this).on('click', function (evt) {
       evt.stopPropagation();
-      console.log("click", $(this).contents());
       overlay($(this).contents());
-      // $(this).css({
-      //   width: "100%",
-      //   height: "100vh",
-      //   "background-color": "white",
-      //   position: "fixed",
-      //   top: 0,
-      //   left: 0,
-      //   "z-index": 999
-      // })
     });
   });
 
-  $("*").mouseout(function () {
-    $(this).css({ "border-color": "inherit",
-      "border-width": "inherit",
-      "border-style": "inherit" });
-    $(this).off( "click");
+  $('*').mouseout(function () {
+    $(this).removeClass('ezrdr-focused');
+    $(this).off( 'click');
   });
 }
 
 const overlay = (content) => {
-  console.log("overlay");
   let overlayDiv = document.createElement('div');
   let closeButton = document.createElement('button');
 
